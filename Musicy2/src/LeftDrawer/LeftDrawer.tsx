9336a9bd2d705323f2f632461e94f71c1488e523
lubabs770 <samgurkov@gmail.com>
2026-02-22 13:55:32 -0500

    Update LeftDrawer.tsx

--- Musicy2/src/LeftDrawer/LeftDrawer.tsx
+++ Musicy2/src/LeftDrawer/LeftDrawer.tsx
@@ -8,10 +8,10 @@ function LeftDrawer({ setMainView }) {
 return (
     <div>
         <ul>
-            <li><button onClick={() => setMainView('Home')}>Home</button></li>
-            <li><button onClick={() => setMainView('Artists')}>Artists</button></li>
-            <li><button onClick={() => setMainView('Scrobbly')}>Scrobbly</button></li>
-            <li><button onClick={() => setMainView('Preferences')}>Preferences</button></li>
+            <li><button className="leftdrawerbutton" onClick={() => setMainView('Home')}>Home</button></li>
+            <li><button className="leftdrawerbutton" onClick={() => setMainView('Artists')}>Artists</button></li>
+            <li><button className="leftdrawerbutton" onClick={() => setMainView('Scrobbly')}>Scrobbly</button></li>
+            <li><button className="leftdrawerbutton" onClick={() => setMainView('Preferences')}>Preferences</button></li>
         </ul>
     </div>
 )

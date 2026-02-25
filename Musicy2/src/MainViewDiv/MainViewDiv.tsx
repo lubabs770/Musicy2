6c6c54347f9d0bc4b1069e4b9c4f8296be812855
lubabs770 <samgurkov@gmail.com>
2026-02-24 20:55:31 -0500

    Update MainViewDiv.tsx

--- Musicy2/src/MainViewDiv/MainViewDiv.tsx
+++ Musicy2/src/MainViewDiv/MainViewDiv.tsx
@@ -1,14 +1,14 @@
 import './MainViewDiv.css'
 import OnEmpty from './onEmpty'
-import Home from './Home'
-import Artists from './Artists'
-import Scrobbly from './Scrobbly'
-import Preferences from './Preferences'
+import Home from './onHome'
+import Artists from './onArtists'
+import Scrobbly from './onScrobbly'
+import Preferences from './onPreferences'
  
 
 function MainViewDiv({ MainView }) {
 
-if (MainView === 'home'){return(<div><Home /></div>)}
+if (MainView === 'Home'){return(<div><Home /></div>)}
 if (MainView === 'Artists'){return(<div><Artists /></div>)}
 if (MainView === 'Scrobbly'){return(<div><Scrobbly /></div>)}
 if (MainView === 'Preferences'){return(<div><Preferences /></div>)}
